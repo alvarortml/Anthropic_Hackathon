5e2756357e8a67aebf3895c654a7a7367356cf65
alvarortml <alortegagon@gmail.com>
2023-11-04 23:01:49 +0000

    Aded json parsing to output of grader, and added finalizing prompt to better encapsulate the conversation

--- src/agents/grader/grader_agent.py
+++ src/agents/grader/grader_agent.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-import pdb
+import json
 
 from dotenv import load_dotenv
 _ = load_dotenv('.env.anthropic')
@@ -9,20 +9,17 @@ _ = load_dotenv('.env.anthropic')
 
 from langchain.chat_models import ChatAnthropic
 from langchain.schema.messages import AIMessage, HumanMessage
+from src.prompts.prompts_grader import GRADER_SYSTEM_PROMPT, GRADER_FINALIZING_PROMPT
 
 if __name__ == "__main__":
 
     chat_model = ChatAnthropic(temperature=0, model = 'claude-2')
-    prompt = [
-        HumanMessage(content=("""You are a grader. Your task is to look at a
-                    conversation between an AI and myself, and assess if the I solved the problem.
-                    Your output should be a json with one key called success, with value 1 if I solved the problem
-                    and value 0 otherwise, and another key called summary, summarizing any issues I had when 
-                    trying to solve the problem. Don't include any other information in your output, onlhy the JSON""")),
-        AIMessage(content=("")),
-        HumanMessage(content=("The solution to the problem is 1 + 1 = 3")),
-        AIMessage(content=("That's incorrect. Check your calculations")),
-        HumanMessage(content="")
+    system_prompt = GRADER_SYSTEM_PROMPT
+    finalizing_prompt = GRADER_FINALIZING_PROMPT
+    conv = [
+        HumanMessage(content=("The solution to the problem is 1 + 1 = 2")),
+        AIMessage(content=("Well done! That's right")),
     ]
-    response = chat_model(prompt).content
-    pdb.set_trace()
+    response = chat_model.invoke(system_prompt + conv + finalizing_prompt)
+    graded_json = json.loads(response.content)
+    print(response)
