30f5ba2f4d363b6ccdebf95416ba7859b00b5df5
alvarortml <alortegagon@gmail.com>
2023-11-05 01:17:58 +0000

    Replace tutor agent with simple conversational agent with better prompt. Added some postparsing for the grader to extract the json when extra stuff is added

--- src/app/pages/two_sum.py
+++ src/app/pages/two_sum.py
@@ -2,22 +2,18 @@ import os
 import streamlit as st
 import json
 from langchain.chat_models import ChatAnthropic
-from src.prompts.prompts_coding_tutor import HUMAN_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER, AI_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER
+from src.prompts.prompts_coding_tutor import HUMAN_TUTOR_WITHOUT_CODE_INTERPRETER, AI_TUTOR_WITHOUT_CODE_INTERPRETER
 from src.utils.problems import LeetCodeProblem
-from langchain_experimental.tools import PythonREPLTool
-from langchain.tools.render import format_tool_to_openai_function
-from langchain.agents.format_scratchpad import format_to_openai_functions
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ConversationBufferMemory
-from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.agents import AgentExecutor
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langsmith import Client
 from langchain.callbacks import LangChainTracer
 from langchain.schema.runnable import RunnableConfig
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
+from langchain.chains import LLMChain
+import re
+
+pattern = r'\{[^}]+\}'
 
 BASIC_PROBLEM_STATEMENT = """
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -49,12 +45,14 @@ if __name__ == "__main__":
     if file_name not in st.session_state.leetcode_problem.keys():
         st.session_state.leetcode_problem[file_name] = LeetCodeProblem(statement=BASIC_PROBLEM_STATEMENT, solution=REFERENCE_SOLUTION)
     
-    if file_name not in st.session_state.system_message.keys():
-        st.session_state.system_message[file_name] = \
-            {"human"  : HUMAN_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER.format(
-            problem_statement = st.session_state.leetcode_problem[file_name].statement),
-            "ai": AI_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER.format(
-                reference_solution = st.session_state.leetcode_problem[file_name].solution)}
+    if file_name not in st.session_state.tutor_agent_human_system_message.keys():
+        st.session_state.tutor_agent_human_system_message[file_name] = \
+            HUMAN_TUTOR_WITHOUT_CODE_INTERPRETER.format(
+            problem_statement = st.session_state.leetcode_problem[file_name].statement)
+        st.session_state.tutor_agent_ai_system_message[file_name] = \
+            AI_TUTOR_WITHOUT_CODE_INTERPRETER.format(
+                reference_solution = st.session_state.leetcode_problem[file_name].solution,
+                problem_statement = st.session_state.leetcode_problem[file_name].statement)
     
     client = Client(api_url=st.session_state.langchain_endpoint, api_key=st.secrets['LANGCHAIN_API_KEY'])
     ls_tracer_tutor = LangChainTracer(project_name=f"{file_name}_tutor", client=client)
@@ -69,34 +67,29 @@ if __name__ == "__main__":
     for msg in msgs.messages:
         st.chat_message(msg.type).write(msg.content)
 
-    print(st.session_state.system_message[file_name]["human"])
-
-    tools = [PythonREPLTool()]
-    functions = [format_tool_to_openai_function(tool) for tool in tools]
     chat_model = ChatAnthropic(model=st.session_state.model, temperature=0, 
-                               anthropic_api_key=st.secrets["ANTHROPIC_API_KEY"],
-                               HUMAN_PROMPT=st.session_state.system_message[file_name]["human"].content,
-                               AI_PROMPT=st.session_state.system_message[file_name]["ai"].content)
+                               anthropic_api_key=st.secrets["ANTHROPIC_API_KEY"])
     
-    agent_chain = initialize_agent(
-        tools=tools,
-        llm=chat_model,
-        agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-        verbose=True,
-        memory=memory,
-    )
+    prompt = ChatPromptTemplate.from_messages([
+        st.session_state.tutor_agent_human_system_message[file_name],
+        st.session_state.tutor_agent_ai_system_message[file_name],
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
+    ])
+    
+    conversation_chain = LLMChain(llm=chat_model, prompt=prompt, verbose=True, memory=memory)
 
     if user_input := st.chat_input("Type here"):
         st.chat_message("user").write(user_input)
-        response = agent_chain.invoke(input= {"input": user_input}, config=RunnableConfig(callbacks=[ls_tracer_tutor]))
-        st.chat_message("assistant").write(response['output'])
-        print(chat_model.AI_PROMPT)
+        response = conversation_chain.invoke(input= {"input": user_input}, config=RunnableConfig(callbacks=[ls_tracer_tutor]))
+        st.chat_message("assistant").write(response['text'])
 
     if st.button('Finish Problem'):
         messages = st.session_state.grader_agent_system_message + msgs.messages + st.session_state.grader_agent_finalizing_message
         grader_response = st.session_state.grader_agent.invoke(messages, config=RunnableConfig(callbacks=[ls_tracer_grader]))
-        graded_json = json.loads(grader_response.content)
-        print(graded_json)
+        matches = re.findall(pattern, grader_response.content)
+        processed_response = matches[0].replace('\n', '')
+        graded_json = json.loads(processed_response)
         file_path = f'/Users/alvaro/Documents/ML_Projects/ANTHROPIC_HACKATHON/src/grades/{file_name}.json'
         with open(file_path, 'w') as json_file:
             json.dump(graded_json, json_file)
