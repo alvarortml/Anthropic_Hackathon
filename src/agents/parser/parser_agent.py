d8b30596d49faf65a30133acc41a44b313b405e6
alvarortml <alortegagon@gmail.com>
2023-11-05 09:53:27 +0000

    Code in development yet, but trying to parse xml output from LLM

--- src/agents/parser/parser_agent.py
+++ src/agents/parser/parser_agent.py
@@ -1,7 +1,6 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-import pdb
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv(".env.anthropic")) # read local .env file
@@ -11,23 +10,40 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import PyPDFLoader
 from langchain_experimental.llms.anthropic_functions import AnthropicFunctions
 from src.utils.attributes import HomeWork
+import xml.etree.ElementTree as ET
+from langchain.chains import create_extraction_chain
 
+start_tag = "<problems>"
+end_tag = "</problems>"
 
+schema = {
+    "properties": {
+        "problem_name": {"type": "string"},
+        "problem_statement" : {"type": "string"},
+        "reference_solution": {"type": "string"},
+    },
+    "required": ["problem_name", "problem_statement", "reference_solution"],
+}
 
 if __name__ == "__main__":
 
     loader = PyPDFLoader("/Users/alvaro/Documents/ML_Projects/ANTHROPIC_HACKATHON/homework/homework.pdf")
     pages = loader.load()
     chat_model = AnthropicFunctions(temperature=0, model = "claude-2", 
-                anthropic_api_key = os.environ['ANTHROPIC_API_KEY'])
-    extract_functions = [convert_pydantic_to_openai_function(HomeWork)]
-    pdb.set_trace()
+                anthropic_api_key = os.environ['ANTHROPIC_API_KEY'], max_tokens = 2000)
     prompt = ChatPromptTemplate.from_messages([
-        ("human", "Extract the relevant information, if not explicitly provided do not guess. Extract partial info"),
+        ("human", "Extract the relevant information"),
         ("ai", ""),
         ("human", "{input}")
     ])
-    pdb.set_trace()
+    chain = create_extraction_chain(schema=schema, llm=chat_model)
+    results = chain.run(pages[0].page_content)
     messages = prompt.format_messages(input=pages[0].page_content)
     response = chat_model(messages=messages, functions=extract_functions)
-    pdb.set_trace()
+    parsed_result = response.content
+    start_index = parsed_result.find(start_tag)
+    end_index = parsed_result.find(end_tag, start_index + len(start_tag) + 1)
+    if start_index != -1 and end_index != -1:
+      xml_string = parsed_result[start_index:end_index + len(end_tag)]
+      xml_string = xml_string.replace('\n', '')
+    response
