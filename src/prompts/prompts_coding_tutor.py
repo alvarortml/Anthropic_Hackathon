04d5f711ce5733b64e59153a72c0dbaba38bace0
alvarortml <alortegagon@gmail.com>
2023-11-05 00:32:44 +0000

    Added new prompts for the coding tutor without the python interpreter. Made emphasis on comparing solution proposed against reference, and introduced lots of xml

--- src/prompts/prompts_coding_tutor.py
+++ src/prompts/prompts_coding_tutor.py
@@ -1,7 +1,7 @@
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, AIMessagePromptTemplate
 
-BASIC_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER = ChatPromptTemplate.from_messages(
-    [("human", """I want to learn how solve the following programming problem:
+HUMAN_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER = HumanMessagePromptTemplate.from_template(
+    """I want to learn how solve the following programming problem:
       <problem>{problem_statement}</problem>
       Your goal is to help me. You have a reference solution to thes problem to do so.
       When I ask you a question, I want you to give me hints about the solution, but never completely reveal the answer,
@@ -22,10 +22,38 @@ BASIC_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER = ChatPromptTempl
                 you should give me a hint on how to improve the runtime of my code
              7: On the other hand, if my code produced some incorrect outputs when running it,
             you should let me know and give me a hint on how to fix my code </steps>
-    """),
-    ("ai", """Hello! I am here to help you learn how to solve the problem. I will only give you 
+    """
+    )
+
+AI_TUTOR_SYSTEM_TEMPLATE_WITH_SOLUTION_AND_CODE_INTERPRETER = AIMessagePromptTemplate.from_template(
+    """Hello! I am here to help you learn how to solve the problem. I will only give you 
      hints, but never the explicit solution, as you need to learn. I will use the following reference
      solution:
     <reference_solution>{reference_solution}</reference_solution>
      and my code interpreter to help. I will follow the steps that you have outlined.
-     """)])
+     """)
+
+HUMAN_TUTOR_WITHOUT_CODE_INTERPRETER = HumanMessagePromptTemplate.from_template("""
+   <instruction>
+   I want to learn how to solve the following programming problem:
+   <problem>{problem_statement}</problem>
+   Your goal is to help me. You have a reference solution for the problem that you should use to
+   compare my code against.
+   When I ask you a question, I want you to give me hints about the solution, but never completely reveal the answer,
+   as I want to learn.
+   </instruction>
+   <instruction>
+   I want you to check if my solution solves the problem we are working on, and if it does it with the same runtime
+   of your reference solution
+   </instruction>
+""")
+
+AI_TUTOR_WITHOUT_CODE_INTERPRETER = AIMessagePromptTemplate.from_template("""
+   Hello! I am here to help you learn how to solve the problem <problem>{problem_statement}</problem>. 
+   I will only give you hints, but never the explicit solution, as you need to learn. I will use the following reference
+   solution:
+   <reference_solution>{reference_solution}</reference_solution>
+   to compare your proposed solution agains. 
+   I will always first verigy if your solution solves the problem we are working on, and if it does, if
+   its runtime is the same as that of the reference solution.
+""")
