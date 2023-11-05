25d6ce440fb12367f011f28b2689c5cd30b866b4
alvarortml <alortegagon@gmail.com>
2023-11-05 09:50:19 +0000

    Binding functions to code interpreter to python tutor

--- src/agents/python_tutor/python_tutor.py
+++ src/agents/python_tutor/python_tutor.py
@@ -6,9 +6,9 @@ import pdb
 from dotenv import load_dotenv
 _ = load_dotenv('.env.anthropic')
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = f"Tracing Walkthrough Anthropic"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+#os.environ["LANGCHAIN_TRACING_V2"] = "true"
+#os.environ["LANGCHAIN_PROJECT"] = f"Tutor Anthropic Original"
+#os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 
 
 from langchain_experimental.tools import PythonREPLTool
@@ -36,10 +36,9 @@ instructions = """
 
 if __name__ == "__main__":
 
-    client = Client()
+    #client = Client()
     tools = [PythonREPLTool()]
     functions = [format_tool_to_openai_function(tool) for tool in tools]
-    pdb.set_trace()
     chat_model = AnthropicFunctions(temperature=0, model = "claude-2", 
                 anthropic_api_key = os.environ['ANTHROPIC_API_KEY'])
     prompt = ChatPromptTemplate.from_messages([
@@ -49,9 +48,9 @@ if __name__ == "__main__":
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ])
     memory = ConversationBufferMemory(return_messages=True, memory_key="chat_history")
-    chain = prompt | chat_model | OpenAIFunctionsAgentOutputParser()
+    chain = prompt | chat_model.bind(function_call={"name": "Python_REPL"}, functions=functions)
     agent_chain = RunnablePassthrough.assign(
         agent_scratchpad = lambda x: format_to_openai_functions(x["intermediate_steps"])
     ) | chain
     agent_executor = AgentExecutor(agent=agent_chain, tools=tools, verbose=True, memory=memory)
-    pdb.set_trace()
+    agent_executor.invoke({"input": instructions})
